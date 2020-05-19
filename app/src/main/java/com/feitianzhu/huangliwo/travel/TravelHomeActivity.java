495efe1c0e616eb357c2fbe13813441bbccec2bc
zhouyishun <zhouyishun@linkcircle.cn>
2020-05-19 14:36:13 +0800

    代码修改团油对接

--- app/src/main/java/com/feitianzhu/huangliwo/travel/TravelHomeActivity.java
+++ app/src/main/java/com/feitianzhu/huangliwo/travel/TravelHomeActivity.java
@@ -1,14 +1,18 @@
 package com.feitianzhu.huangliwo.travel;
 
-import android.widget.ImageView;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
 import com.feitianzhu.huangliwo.R;
 import com.feitianzhu.huangliwo.common.base.activity.BaseActivity;
-import com.feitianzhu.huangliwo.utils.GlideUtils;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 /**
@@ -20,10 +24,17 @@ import butterknife.OnClick;
  */
 public class TravelHomeActivity extends BaseActivity {
 
-    @BindView(R.id.imageView)
-    ImageView imageView;
     @BindView(R.id.title_name)
     TextView titleName;
+    @BindView(R.id.distance)
+    TextView distance;
+    @BindView(R.id.oilnumber)
+    TextView oilnumber;
+    @BindView(R.id.oilrecy)
+    RecyclerView oilrecy;
+    private PopupWindow popupWindow;
+    private String all = "";
+    private String num;
 
     @Override
     protected int getLayoutId() {
@@ -32,8 +43,8 @@ public class TravelHomeActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-        titleName.setText("旅游");
-        Glide.with(this).load(R.mipmap.lvyou).into(GlideUtils.getImageView2(this, R.mipmap.lvyou, imageView));
+        titleName.setText("团油");
+        //Glide.with(this).load(R.mipmap.lvyou).into(GlideUtils.getImageView2(this, R.mipmap.lvyou, imageView));
     }
 
     @OnClick(R.id.left_button)
@@ -44,5 +55,79 @@ public class TravelHomeActivity extends BaseActivity {
     @Override
     protected void initData() {
 
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.distance, R.id.oilnumber})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.distance:
+                view = LayoutInflater.from(TravelHomeActivity.this).inflate(
+                        R.layout.popup_item_distance, null);
+                popupWindow = new PopupWindow(view,
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT, true);
+                TextView oilnameone = view.findViewById(R.id.distanceone);
+                TextView oilnametwodiesel = view.findViewById(R.id.distancetwo);
+                TextView oilnametendiesel = view.findViewById(R.id.distancethree);
+                TextView oilnametwo = view.findViewById(R.id.distancefour);
+                oilnameone.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        all = "5km";
+                        distance.setText(all);
+                        num =all.substring(0,all.length()-1);
+                        int page=0;
+                        popupWindow.dismiss();
+                    }
+                });
+                oilnametwodiesel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        all = "10km";
+                        distance.setText(all);
+                        num =all.substring(0,all.length()-1);
+                        int page=0;
+                        popupWindow.dismiss();
+                    }
+                });
+                oilnametendiesel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        all = "20km";
+                        distance.setText(all);
+                        num =all.substring(0,all.length()-1);
+                        int page=0;
+                        popupWindow.dismiss();
+                    }
+                });
+                oilnametwo.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        all = "30km";
+                        distance.setText(all);
+                        num =all.substring(0,all.length()-1);
+                        int page=0;
+                        popupWindow.dismiss();
+                    }
+                });
+                //popupWindow.setAnimationStyle(R.style.MyPopupWindow_anim_style);
+                // popupWindow.showAtLocation(oilnumber, Gravity.TOP, 1,1);
+                popupWindow.showAsDropDown(oilnumber);
+                break;
+            case R.id.oilnumber:
+                view = LayoutInflater.from(TravelHomeActivity.this).inflate(
+                        R.layout.popup_item_oil, null);
+                popupWindow = new PopupWindow(view,
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT, true);
+                break;
+        }
     }
 }
