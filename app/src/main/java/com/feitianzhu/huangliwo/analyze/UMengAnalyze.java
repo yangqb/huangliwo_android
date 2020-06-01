14f3ef417c32fed85ead4b23a344803a4aba2564
bch <15011462696@163.com>
2020-06-01 11:52:15 +0800

    去除友盟推送,准备合并

--- app/src/main/java/com/feitianzhu/huangliwo/analyze/UMengAnalyze.java
+++ app/src/main/java/com/feitianzhu/huangliwo/analyze/UMengAnalyze.java
@@ -7,8 +7,6 @@ import com.google.gson.Gson;
 import com.umeng.analytics.MobclickAgent;
 import com.umeng.commonsdk.UMConfigure;
 import com.umeng.commonsdk.statistics.common.DeviceConfig;
-import com.umeng.message.IUmengRegisterCallback;
-import com.umeng.message.PushAgent;
 
 import java.util.logging.Handler;
 
@@ -50,23 +48,23 @@ public class UMengAnalyze {
 //        UMConfigure.init(context, APPKEY, Channel, UMConfigure.DEVICE_TYPE_PHONE, MessageSecret);
 //        getTestDeviceInfo(context);
         //获取消息推送代理示例
-        PushAgent mPushAgent = PushAgent.getInstance(context);
-
-
-//注册推送服务，每次调用register方法都会回调该接口
-        mPushAgent.register(new IUmengRegisterCallback() {
-            @Override
-            public void onSuccess(String deviceToken) {
-                UMengAnalyze.this.deviceToken = deviceToken;
-                //注册成功会返回deviceToken deviceToken是推送消息的唯一标志
-                Log.i(TAG, "注册成功：deviceToken：-------->  " + deviceToken);
-            }
-
-            @Override
-            public void onFailure(String s, String s1) {
-                Log.e(TAG, "注册失败：-------->  " + "s:" + s + ",s1:" + s1);
-            }
-        });
+//        PushAgent mPushAgent = PushAgent.getInstance(context);
+//
+//
+////注册推送服务，每次调用register方法都会回调该接口
+//        mPushAgent.register(new IUmengRegisterCallback() {
+//            @Override
+//            public void onSuccess(String deviceToken) {
+//                UMengAnalyze.this.deviceToken = deviceToken;
+//                //注册成功会返回deviceToken deviceToken是推送消息的唯一标志
+//                Log.i(TAG, "注册成功：deviceToken：-------->  " + deviceToken);
+//            }
+//
+//            @Override
+//            public void onFailure(String s, String s1) {
+//                Log.e(TAG, "注册失败：-------->  " + "s:" + s + ",s1:" + s1);
+//            }
+//        });
 
 
     }
